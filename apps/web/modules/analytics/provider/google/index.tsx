4f1776126e066b5850fbe4c3ebfe3a5abcc1f056
imaxwel <1457735+imaxwel@users.noreply.github.com>
2025-05-31 10:15:26 +0800

    try to fix ga4 not working

--- apps/web/modules/analytics/provider/google/index.tsx
+++ apps/web/modules/analytics/provider/google/index.tsx
@@ -1,41 +1,49 @@
 "use client";
-
 import Script from "next/script";
 
 const googleTagId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string;
 
 export function AnalyticsScript() {
-	return (
-		<Script
-			async
-			src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
-			onLoad={() => {
-				if (typeof window === "undefined") {
-					return;
-				}
-
-				(window as any).dataLayer = (window as any).dataLayer || [];
-
-				function gtag(...rest: any[]) {
-					(window as any).dataLayer.push(...rest);
-				}
-				gtag("js", new Date());
-				gtag("config", googleTagId);
-			}}
-		/>
-	);
+    return (
+        <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+            onLoad={() => {
+                if (typeof window === "undefined") {
+                    return;
+                }
+                
+                // 初始化 dataLayer
+                (window as any).dataLayer = (window as any).dataLayer || [];
+                
+                // 定义 gtag 函数
+                function gtag(...args: any[]) {
+                    (window as any).dataLayer.push(arguments);
+                }
+                
+                // 将 gtag 函数挂载到 window 对象
+                (window as any).gtag = gtag;
+                
+                // 初始化 GA
+                gtag("js", new Date());
+                gtag("config", googleTagId, {
+                    page_title: document.title,
+                    page_location: window.location.href
+                });
+            }}
+        />
+    );
 }
 
 export function useAnalytics() {
-	const trackEvent = (event: string, data?: Record<string, unknown>) => {
-		if (typeof window === "undefined" || !(window as any).gta) {
-			return;
-		}
-
-		(window as any).gta("event", event, data);
-	};
-
-	return {
-		trackEvent,
-	};
+    const trackEvent = (event: string, data?: Record<string, unknown>) => {
+        if (typeof window === "undefined" || !(window as any).gtag) {
+            return;
+        }
+        (window as any).gtag("event", event, data);
+    };
+    
+    return {
+        trackEvent,
+    };
 }
