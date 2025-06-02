59f9b4cfd687a83d5eb0e154a58c93795c36ffa1
imaxwel <1457735+imaxwel@users.noreply.github.com>
2025-06-02 10:39:14 +0800

    using gemini-2.0-flash-exp

--- packages/ai/index.ts
+++ packages/ai/index.ts
@@ -1,8 +1,11 @@
-import { anthropic } from "@ai-sdk/anthropic";
+// import { anthropic } from "@ai-sdk/anthropic";
+import { google } from "@ai-sdk/google";
+
  
 import { openai } from "@ai-sdk/openai";
 
-export const textModel = anthropic("claude-3-5-sonnet-20240620");
+// export const textModel = anthropic("claude-3-5-sonnet-20240620");
+export const textModel = google("models/gemini-2.0-flash-exp");
 // export const textModel = openai("gpt-4o-mini");
 export const imageModel = openai("dall-e-3");
 export const audioModel = openai("whisper-1");
